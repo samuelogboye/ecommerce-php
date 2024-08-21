53b18152955f47ebb2dcbd27eb868811861301c4
Samuel Ogboye <ogboyesam@gmail.com>
2024-08-22 00:57:52 +0100

    fix(lint): fix lint error

--- routes/api.php
+++ routes/api.php
@@ -45,7 +45,7 @@ Route::group([
     'middleware' => 'api',
     'prefix' => 'auth'
 
-], function ($router) {
+], function () {
     Route::post('/register', [AuthController::class, 'register']);
     Route::post('/login', [AuthController::class, 'login']);
     Route::get('/profile', [AuthController::class, 'profile']);
