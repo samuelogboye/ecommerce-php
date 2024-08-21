bba9337d9d4c4ba5da9be9ca61ff662a55fb4e9c
Samuel Ogboye <ogboyesam@gmail.com>
2024-08-22 00:57:09 +0100

    feat(logic): updated view controller

--- app/Http/Controllers/ViewController.php
+++ app/Http/Controllers/ViewController.php
@@ -3,13 +3,19 @@
 namespace App\Http\Controllers;
 
 use App\Models\View;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
 
 class ViewController extends Controller
 {
     public function index()
     {
-        return View::with('product')->get();
+        $views =  View::with('product')->get();
+        return response()->json([
+            'message'=> 'All views retrieved',
+            'data'=>$views,
+            'count'=>count($views)
+        ], 200);
     }
 
     public function store(Request $request)
@@ -20,7 +26,11 @@ class ViewController extends Controller
 
     public function show($id)
     {
-        return View::with('product')->findOrFail($id);
+        try {
+            return View::with('product')->findOrFail($id);
+        } catch (ModelNotFoundException $e) {
+            return response()->json(['message' => 'View not found'], 404);
+        }
     }
 
     public function update(Request $request, $id)
