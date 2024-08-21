3a943c5bc84f0e6e20848116833901c152a335a6
Samuel Ogboye <ogboyesam@gmail.com>
2024-08-22 00:57:25 +0100

    feat(logic): updated product controller

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -9,30 +9,23 @@ class Product extends Model
 {
     use HasFactory;
 
-    protected $table = 'product';
-
     protected $fillable = [
         'name', 'description', 'qty', 'price', 'category_id', 'subcategory_id', 'featured_image', 'rank', 'status',
     ];
 
     public function category()
     {
-        return $this->belongsTo(Category::class);
+        return $this->belongsTo(Category::class, 'category_id');
     }
 
     public function subCategory()
     {
-        return $this->belongsTo(SubCategory::class);
+        return $this->belongsTo(SubCategory::class, 'subcategory_id');
     }
 
     public function tags()
     {
-        return $this->belongsToMany(Tag::class, 'product_tag');
-    }
-
-    public function orderItems()
-    {
-        return $this->hasMany(OrderItem::class);
+        return $this->belongsToMany(Tag::class, 'product_tags');
     }
 
     public function views()
