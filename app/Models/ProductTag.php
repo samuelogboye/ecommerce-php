cb5dfb454189f47c8b76839fcc4e8ba1c7096f2e
Samuel Ogboye <ogboyesam@gmail.com>
2024-08-22 00:57:34 +0100

    feat(logic): updated producttag controller

--- app/Models/ProductTag.php
+++ app/Models/ProductTag.php
@@ -6,7 +6,5 @@ use Illuminate\Database\Eloquent\Relations\Pivot;
 
 class ProductTag extends Pivot
 {
-    protected $table = 'product_tag';
-
     protected $fillable = ['product_id', 'tag_id'];
 }
